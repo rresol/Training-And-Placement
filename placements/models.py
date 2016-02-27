b1e67c30b083a39bc7ecab7f2d085bc17d5ccd64
shashank.kumar.apc13@itbhu.ac.in <shashank.kumar.apc13@itbhu.ac.in>
2016-02-28 01:03:40 +0530

    Initialised the repository for TPO site

---
+++ placements/models.py
@@ -0,0 +1,8 @@
+__author__ = 'rresol'
+__email__  = 'shashank.kumar.apc13@itbhu.ac.in'
+
+from __future__ import unicode_literals
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
